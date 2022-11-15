32cda645a6d11e9e888e0b3b49054574ac82b4b5
Sebastian Röder <sebroeder@users.noreply.github.com>
2022-11-15 22:16:01 +0100

    Add current time to guess live view

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -3,7 +3,7 @@ defmodule PentoWeb.WrongLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: current_time())}
   end
 
   @impl true
@@ -11,9 +11,17 @@ defmodule PentoWeb.WrongLive do
     ~H"""
     <h1 class="text-2xl">Your score: <%= @score %></h1>
     <h2 class="text-lg"><%= @message %></h2>
+    <p>Current time: <%= @time %></p>
     <h2>
       <%= for n <- 1..10 do %>
-        <a href="#" phx-click="guess" phx-value-number={n} class="hover:text-orange-500 hover:underline"><%= n %></a>
+        <a
+          href="#"
+          phx-click="guess"
+          phx-value-number={n}
+          class="hover:text-orange-500 hover:underline"
+        >
+          <%= n %>
+        </a>
       <% end %>
     </h2>
     """
@@ -24,6 +32,8 @@ defmodule PentoWeb.WrongLive do
     message = "Your guess: #{guess}. Wrong. Guess again!"
     score = socket.assigns.score - 1
 
-    {:noreply, assign(socket, message: message, score: score)}
+    {:noreply, assign(socket, message: message, score: score, time: current_time())}
   end
+
+  defp current_time, do: Time.utc_now() |> to_string
 end
